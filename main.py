a29d32c3940995a5fcd58249479e204590009191
garich82 <garo@gbg.bg>
2023-05-31 11:41:52 +0100

    Optimizing the whole code, removing unnecessary function and reducing the code from 174 to 155 lines.

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import random
 
 
 def generate_secret_number():
-    """Generate a random 4-digit secret number for the computer"""
-    digits = random.sample(range(10), 4)
-    while digits[0] == 0:
+    while True:
         digits = random.sample(range(10), 4)
-    return digits
+        if digits[0] != 0:
+            return digits
 
 
 def player_secret_number():
-    """Validate player's number"""
     while True:
         player_number = input("Enter your secret number (4 unique digits): ")
         if len(player_number) != 4 or not player_number.isdigit():
@@ -19,31 +17,23 @@ def player_secret_number():
 
         digits = [int(i) for i in player_number]
         if len(set(digits)) != 4 or digits[0] == 0:
-            print("Number should not start with 0 and/or digits must not repeat!")
+            print("Number should not start with 0 and digits must not repeat!")
             continue
 
         return digits
 
 
 def get_user_guess():
-    """Get a 4-digit guess from the user."""
     while True:
         guess = input("Enter your guess (4 digits): ")
-        if len(guess) != 4 or not guess.isdigit():
-            print("Invalid guess. Please enter a 4-digit number.")
+        if len(guess) != 4 or not guess.isdigit() or guess[0] == '0':
+            print("Invalid guess. Please enter a 4-digit number without leading zero.")
         else:
             return list(map(int, guess))
 
 
-def has_repeated_digits(digits, guesses):
-    for guess in guesses.keys():
-        guess_digits = list(guess)
-        if all(guess_digits.count(digit) == digits.count(digit) for digit in digits):
-            return True
-    return False
-
-
-def compare_positions(digits, all_cows_numbers):
+def compare_positions(digits, guesses):
+    all_cows_numbers = [key for key, value in guesses.items() if value == "0B4C"]
     for cow_number in all_cows_numbers:
         for i in range(4):
             if digits[i] == cow_number[i]:
@@ -53,36 +43,29 @@ def compare_positions(digits, all_cows_numbers):
 
 def get_computer_guess(possible_digits, sure_digits, guesses):
     if len(possible_digits) == 4 and "0B4C" in guesses.values():
-        all_cows_numbers = []
-        for key, value in guesses.items():
-            if value == "0B4C":
-                all_cows_numbers.append(key)
+        #  Special scenario where there are only 4 numbers left with 4 cows already found
         digits = random.sample(possible_digits, k=4)
-        while digits[0] == 0 or tuple(digits) in guesses or compare_positions(digits, all_cows_numbers):
-            digits = random.sample(possible_digits, k=4)
-    elif len(possible_digits) == 4:
-        digits = random.sample(possible_digits, k=4)
-        while digits[0] == 0 or tuple(digits) in guesses:
+        while digits[0] == 0 or tuple(digits) in guesses or compare_positions(digits, guesses):
             digits = random.sample(possible_digits, k=4)
     elif sure_digits:
+        #  Special scenario where the sure digits are always used
         remaining_digits = random.sample([digit for digit in possible_digits if digit not in sure_digits], k=2)
         digits = sure_digits + remaining_digits
         random.shuffle(digits)
-        while digits[0] == 0 or tuple(digits) in guesses or has_repeated_digits(digits, guesses):
+        while digits[0] == 0 or tuple(digits) in guesses:
             remaining_digits = random.sample([digit for digit in possible_digits if digit not in sure_digits], k=2)
             digits = sure_digits + remaining_digits
             random.shuffle(digits)
     else:
+        #  All other scenarios
         digits = random.sample(possible_digits, k=4)
-        while digits[0] == 0 or int(''.join(map(str, digits))) in guesses:
+        while digits[0] == 0 or tuple(digits) in guesses:
             digits = random.sample(possible_digits, k=4)
     return digits
 
 
 def calculate_bulls_and_cows(secret_number, guess):
-    """Calculate the number of bulls and cows in the guess."""
-    bulls = 0
-    cows = 0
+    bulls = cows = 0
 
     for i in range(len(guess)):
         if guess[i] == secret_number[i]:
@@ -109,8 +92,7 @@ def play_game():
 
         if bulls == 4:
             print(f"\nCongratulations! You guessed computer's number with {counter} attempts\n")
-            return[0, 1]
-            break
+            return [0, 1]
 
         print(f"You have {bulls} bulls and {cows} cows")
 
@@ -132,8 +114,8 @@ def play_game():
             digits_to_remove = computer_guess
             sure_digits = [digit for digit in possible_digits if digit not in digits_to_remove]
 
-        computer_guess_int = computer_guess[0] * 1000 + computer_guess[1] * 100\
-                                                  + computer_guess[2] * 10 + computer_guess[3]
+        computer_guess_int = computer_guess[0] * 1000 + computer_guess[1] * 100 \
+                             + computer_guess[2] * 10 + computer_guess[3]
         print(f"Computer guessed {computer_guess_int} and got {comp_bulls} bulls and {comp_cows} cows")
 
 
@@ -145,8 +127,7 @@ print("For every digit in each guess that matches the secret number in the corre
 print("For every digit in each guess that matches the secret number but in the wrong position, that's a cow.")
 print("Let's start!")
 
-computer_wins = 0
-player_wins = 0
+computer_wins = player_wins = 0
 
 while True:
     computer_win, player_win = play_game()
@@ -158,17 +139,17 @@ while True:
 
     end_of_game = False
     while True:
-        user_input = input("Do you want to continue? (Y)es or (N)o: ")
+        user_input = input("Do you want to continue? (Y)es or (N)o: ").lower()
 
-        if user_input.lower() == 'y' or user_input.lower() == 'yes':
+        if user_input in ['y', 'yes']:
             print("You chose to continue.\n")
             break
-        elif user_input.lower() == 'n' or user_input.lower() == 'no':
+        elif user_input in ['n', 'no']:
             print("You chose to stop. This is tne end of the game!")
             end_of_game = True
             break
         else:
-            print("Invalid input. Please enter either 'Y' or 'N'.")
+            print("Invalid input. Please enter either 'Y' to continue or 'N' to stop playing.")
 
     if end_of_game:
         break
